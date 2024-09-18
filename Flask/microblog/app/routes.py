851cd2cecf7105108e2f1ac06f930fcedbfd0765
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2024-09-18 12:30:30 -0300

    book: cap-3 finished

--- Flask/microblog/app/routes.py
+++ Flask/microblog/app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 
 @app.route("/")
@@ -15,4 +16,18 @@ def index():
         {"author": {"name": "Antonio"}, "title": "How to make new friends?",
          "body": "Reading a book in english is better than reading a book in english.", "created": "08/09/2024"}
     ]
-    return render_template("indexw.html", title="Home", user=user, posts=posts)
+    return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    print(form.validate_on_submit())
+    print(form.errors)
+    print(form.data)
+    print(form.remember_me)
+    if form.validate_on_submit():
+        print("is login successful?")
+        flash(f"Login requested for {form.username} remember_me={form.remember_me.data}")
+        return redirect("/index")
+    return render_template("login.html", title="Login", form=form)
