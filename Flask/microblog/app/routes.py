856df0cb11541c39e44f48376553d1170dc79f11
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2024-09-18 08:56:22 -0300

    book: ref - cap-2 templates

--- Flask/microblog/app/routes.py
+++ Flask/microblog/app/routes.py
@@ -6,5 +6,13 @@ from app import app
 @app.route("/index")
 def index():
     user = {"username": "Marcos", "email": "teste@teste.com"}
-    return render_template("index.html", title="Home", user=user)
-
+    posts = [
+        {"author": {"name": "Luiza"}, "title": "How to read a book in english",
+         "body": "Reading a book in english is better than reading a book in english.",
+         "created": "12/06/2024"},
+        {"author": {"name": "Marcos"}, "title": "How can i say this sentences in english?",
+         "body": "Reading a book in english is better than reading a book in english.", "created": "12/06/2024"},
+        {"author": {"name": "Antonio"}, "title": "How to make new friends?",
+         "body": "Reading a book in english is better than reading a book in english.", "created": "08/09/2024"}
+    ]
+    return render_template("indexw.html", title="Home", user=user, posts=posts)
