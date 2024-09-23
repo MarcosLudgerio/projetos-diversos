b62eb453b51ea087eb0538b291a44edcbc354e3f
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2024-09-23 07:52:14 -0300

    book: cap-4 introduce database

---
+++ Flask/microblog/app/models.py
@@ -0,0 +1,25 @@
+from app import db
+from datetime import datetime
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=True)
+    email = db.Column(db.String(120), unique=True, index=True)
+    password = db.Column(db.String(80))
+
+    def __repr__(self):
+
+        return '<User %r>' % self.username
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    body = db.Column(db.String(1000), nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user'))
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
