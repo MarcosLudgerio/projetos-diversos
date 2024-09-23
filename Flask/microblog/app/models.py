edbd6f47515b032855d8d6eb0bc331a89187b05c
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2024-09-23 09:50:55 -0300

    book: cap-4 implemented router login and not working yet

--- Flask/microblog/app/models.py
+++ Flask/microblog/app/models.py
@@ -1,16 +1,29 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
 
-class User(db.Model):
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=True)
     email = db.Column(db.String(120), unique=True, index=True)
     password = db.Column(db.String(80))
 
     def __repr__(self):
-
         return '<User %r>' % self.username
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 
 class Post(db.Model):
@@ -18,7 +31,7 @@ class Post(db.Model):
     title = db.Column(db.String(80), nullable=False)
     body = db.Column(db.String(1000), nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Post %r>' % self.title
--- Flask/microblog/app/routes.py
+++ Flask/microblog/app/routes.py
@@ -1,12 +1,15 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
+from flask_login import login_user, current_user, logout_user, login_required
+from app.models import User
+from urllib.parse import urlparse
 
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
-    user = {"username": "Marcos", "email": "teste@teste.com"}
     posts = [
         {"author": {"name": "Luiza"}, "title": "How to read a book in english",
          "body": "Reading a book in english is better than reading a book in english.",
@@ -16,18 +19,33 @@ def index():
         {"author": {"name": "Antonio"}, "title": "How to make new friends?",
          "body": "Reading a book in english is better than reading a book in english.", "created": "08/09/2024"}
     ]
-    return render_template("index.html", title="Home", user=user, posts=posts)
+    return render_template("index.html", title="Home", posts=posts)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        print('--- VERIFICANDO USER AUTHENTICADO ---')
+        return redirect(url_for('index'))
+
     form = LoginForm()
-    print(form.validate_on_submit())
-    print(form.errors)
-    print(form.data)
-    print(form.remember_me)
+    print('--- gerando o form login ---')
     if form.validate_on_submit():
-        print("is login successful?")
-        flash(f"Login requested for {form.username} remember_me={form.remember_me.data}")
-        return redirect("/index")
+        user = User.query.filter_by(username=form.username.data).first()
+        print(f'--- usuario validado {user} ---')
+        if user is not None and user.verify_password(form.password.data):
+            flash('Invalid username or password.')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlparse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    print("--- finalizando o processo ---")
     return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
