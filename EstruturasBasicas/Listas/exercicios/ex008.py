8429f7ee2d071cf91dc7e7c96b22aabc418dd921
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2024-10-29 07:59:53 -0300

    feat: implemented a new examplo using Stack

---
+++ EstruturasBasicas/Listas/exercicios/ex008.py
@@ -0,0 +1,31 @@
+# Problema do parenteses balanceados
+from PilhaImplementacao import Pilha
+
+
+def parentesesCheck(cadeiaString):
+    pilhaParentese = Pilha()
+    balanceada = True
+    index = 0
+    while index < len(cadeiaString) and balanceada:
+        caractere = cadeiaString[index]
+        if caractere == "(":
+            pilhaParentese.empilha(caractere)
+        else:
+            if pilhaParentese.esta_vazia():
+                balanceada = False
+            else:
+                pilhaParentese.desempilha()
+
+        index = index + 1
+
+    if balanceada and s.esta_vazia():
+        return True
+    else:
+        return False
+
+
+parenteses = input("Informe a expressão: ").split(" ")
+if parentesesCheck(parenteses):
+    print("Está balanceado")
+else:
+    print("Não está balanceado")
